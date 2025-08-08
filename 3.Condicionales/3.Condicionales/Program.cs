68cdca88f4c4518a9b700881799505d7c5a40197
Manuel Cardenas <mancar1125@gmail.com>
2025-08-08 09:19:43 -0500

    Condicionales simples falta un ejercicio para la proxima clase

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condicionales simples
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingresa el nombre");
+            nombre = Console.ReadLine(); //Todo lo que se lee desde la consola llega como un string
+
+            Console.WriteLine("Ingresa el sueldo");
+            sueldo = Convert.ToInt32( Console.ReadLine());
+
+            if (sueldo>3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            
+            
+        }
+    }
+}
