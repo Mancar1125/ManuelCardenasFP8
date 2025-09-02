55b348ff587a1a825960ab1df8b9eaeb015b4ac9
Mancar1125 <mancar1125@gmail.com>
2025-09-01 23:45:40 -0500

    Add files via upload

---
+++ TareaAlgoritmo/TareaAlgoritmo/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TareaAlgoritmo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            int numero1 = 0;
+            int numero2 = 0;
+
+            Console.WriteLine("Ingresa tu edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Welcome to my dirty side");
+            }
+            else
+            {
+                Console.WriteLine("No te queremos aqui abusadol");
+            }
+
+            Console.WriteLine("Ingresa Numero 1");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa Numero 2");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine(numero1 + numero2);
+                Console.WriteLine(numero1 - numero2);
+            }
+
+            else
+            {
+                Console.WriteLine(numero1 * numero2);
+                Console.WriteLine(numero1 / numero2);
+            }
+
+
+
+        } 
+    }
+}
