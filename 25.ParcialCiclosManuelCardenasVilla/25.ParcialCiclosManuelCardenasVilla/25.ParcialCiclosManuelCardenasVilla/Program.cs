b307b5d2de49febbb7aa9803d127e5a3167c415f
Mancar1125 <mancar1125@gmail.com>
2025-10-07 23:40:05 -0500

    Add files via upload

---
+++ 25.ParcialCiclosManuelCardenasVilla/25.ParcialCiclosManuelCardenasVilla/25.ParcialCiclosManuelCardenasVilla/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace _25.ParcialCiclosManuelCardenasVilla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //En el programa de Ingeniería Mecánica, que cuenta con 20 estudiantes, se desea procesar información sobre la ocupación y el género de los alumnos.El programa debe realizar las siguientes acciones: Ingresar por teclado la ocupación de cada estudiante(trabaja o estudia) y su género(hombre o mujer). Calcular y mostrar el porcentaje de estudiantes que trabajan y el porcentaje de los que estudian respecto al total del grupo. Calcular y mostrar el número de mujeres que trabajan. Calcular y mostrar el número de hombres que estudian.
+
+            int totalEstudiantes = 5;
+            int estudiantesQueTrabajan = 0;
+            int estudiantesQueEstudian = 0;
+            int mujeresQueTrabajan = 0;
+            int hombresQueEstudian = 0;
+            string ocupacion;
+            string genero;
+
+            for (int contador = 1; contador <= totalEstudiantes; contador++)
+            {
+                
+                do
+                {
+                    Console.WriteLine($"Ingrese la ocupación del estudiante {contador} (trabaja/estudia): ");
+                    ocupacion = Console.ReadLine().ToLower();
+
+                    if (ocupacion != "trabaja" && ocupacion != "estudia")
+                    {
+                        Console.WriteLine("Ocupación inválida. Por favor, ingrese 'trabaja' o 'estudia'.");
+                    }
+
+                } while (ocupacion != "trabaja" && ocupacion != "estudia");
+
+                
+                do
+                {
+                    Console.WriteLine($"Ingrese el género del estudiante {contador} (hombre/mujer): ");
+                    genero = Console.ReadLine().ToLower();
+
+                    if (genero != "hombre" && genero != "mujer")
+                    {
+                        Console.WriteLine("Género incorrecto. Por favor, ingrese 'hombre' o 'mujer'.");
+                    }
+
+                } while (genero != "hombre" && genero != "mujer");
+
+                
+                if (ocupacion == "trabaja")
+                {
+
+                    estudiantesQueTrabajan++;
+                    if (genero == "mujer")
+                    {
+                        mujeresQueTrabajan++;
+                    }
+                }
+                else if (ocupacion == "estudia")
+                {
+                    estudiantesQueEstudian++;
+                    if (genero == "hombre")
+                    {
+                        hombresQueEstudian++;
+
+                    }
+                }
+            }
+
+            double porcentajeQueTrabajan = (double)estudiantesQueTrabajan / totalEstudiantes * 100;
+            double porcentajeQueEstudian = (double)estudiantesQueEstudian / totalEstudiantes * 100;
+
+            Console.WriteLine($"Porcentaje de estudiantes que trabajan: {porcentajeQueTrabajan:F2}%");
+            Console.WriteLine($"Porcentaje de estudiantes que estudian: {porcentajeQueEstudian:F2}%");
+            Console.WriteLine($"Número de mujeres que trabajan: {mujeresQueTrabajan}");
+            Console.WriteLine($"Número de hombres que estudian: {hombresQueEstudian}");
+
+
+        }
+    }
+}
---
+++ 26. Matrices/26. Matrices/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _26._Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numeros = new int[2, 2];
+            string[,] nombres = new string[2, 3]; //rango=6 posiciones
+            
+
+            //Asignacion de valores
+            numeros[0, 0] = 25;
+            numeros[0, 1] = 46;
+            numeros[1, 0] = 58;
+            numeros[1, 1] = 12;
+
+            //Otra forma de declarar e inicializar una matriz
+            string[,] apellidos =
+                {
+                {"Cárdenas", "Villa" },
+                {"Alfaro", "Quijano"},
+                {"Escobar", "Vanegas"},
+                };
+
+            //Recuperar datos de la matriz
+            Console.WriteLine(apellidos[1, 1]);
+
+            //Recorrer una matriz - almacenar datos
+            char[,] caracteres = new char[3, 2]; //rango=6 posiciones
+
+            for (int i = 0; i < 3; i++) //Recorre las filas
+            {
+                for (int j = 0; j < 2; j++) //Recorre las columnas de cada una de las filas
+                {
+                    Console.WriteLine($"Ingrese el caracter para la posición [{i},{j}]: ");
+                    caracteres[i, j] = char.Parse(Console.ReadLine());
+                }
+            }
+
+            //Recorrer una matriz - recuperar datos
+            for (int i = 0; i < caracteres.GetLength(0); i++) //.GetLength(0) devuelve el número de filas
+            {
+                for (int j = 0; j < caracteres.GetLength(1); j++) //.GetLength(1) devuelve el número de columnas
+                {
+                    Console.Write($"Caracter posición {i}, {j}: {caracteres[i,j]}");
+                }
+                Console.WriteLine();
+            }
+
+            //Crear una matriz de 4x4 y llenarla con el número 10
+            int[,] matrizDiez = new int[4, 4];
+            for (int i = 0; i < matrizDiez.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizDiez.GetLength(1); j++)
+                {
+                    matrizDiez[i, j] = 10;
+                    Console.Write($"{matrizDiez[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
