64eea15da9f25f2487450c40e2922cce936f0631
Mancar1125 <mancar1125@gmail.com>
2025-10-07 23:33:40 -0500

    Add files via upload

---
+++ 14.While6/14.While6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _14.While6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //14.Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n.
+            Console.WriteLine("Ingrese un número entero positivo:");
+            int n = int.Parse(Console.ReadLine());
+            int contadorPares = 0;
+            int contador = 1;
+            while (contador <= n)
+            {
+                if (contador % 2 == 0)
+                {
+                    contadorPares++;
+                }
+                contador++;
+
+            }
+            Console.WriteLine($"Hay {contadorPares} números pares entre 1 y {n}.");
+        }
+    }
+}
---
+++ 15.While7/15.While7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Emit;
+
+namespace _15.While7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //15.Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero; además, se quiere saber cuánto lleva ahorrado cada mes. 
+            
+            int mes = 1;
+            double ahorroMensual = 0;
+            double ahorroTotal = 0;
+
+            while (mes <= 12)
+            {
+                Console.Write("Ingrese la cantidad de dinero que ahorró en el mes " + mes + ": ");
+                ahorroMensual = Convert.ToDouble(Console.ReadLine());
+                ahorroTotal += ahorroMensual;
+                Console.WriteLine("El ahorro total hasta el mes " + mes + " es: " + ahorroTotal);
+                mes++;
+            }
+        }
+    }
+}
