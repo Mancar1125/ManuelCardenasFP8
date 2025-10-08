f12194fa16412e6bf153361dc85d72de8288f305
Mancar1125 <mancar1125@gmail.com>
2025-10-07 23:31:25 -0500

    Add files via upload

---
+++ 12.CicloWhile/12.CicloWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.
+            int suma = 0;
+            int contador = 1;
+            while (contador < 100)
+            {
+                suma += contador;
+                Console.WriteLine($"{contador} - {suma}");
+                contador += 2;
+            }
+            Console.WriteLine($"La suma de todos los números impares hasta el 99 es: {suma}");
+
+        }
+    }
+}
